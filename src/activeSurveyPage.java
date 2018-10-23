21d8fb7b78e0e616613685ff5a1a3064d15e1443
Simon <simon.m.houlberg@hotmail.com>
2018-10-23 15:23:32 +0200

    Added the activeSurveyPage, no functions on it yet

---
+++ src/activeSurveyPage.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class activeSurveyPage {
+    private JPanel panel1;
+    private JLabel titleField;
+    private JPanel questionPanel;
+    private JButton backButton;
+}
